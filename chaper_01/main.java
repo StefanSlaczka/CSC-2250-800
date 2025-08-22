6554efb6f5b8c49c6e2924765465cfd22687a615
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2025-08-22 08:44:23 -0400

    thoughs on why this is happening

--- chaper_01/main.java
+++ chaper_01/main.java
@@ -1,5 +1,17 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // I think '0' is a 48 chareter and sents were adding it the output will come out as a number
+        System.out.println('0' + 0);
+        System.out.println('0' + '0');
+        // I think this is happening becasue the program catches its self on the "0" or just adds it as a string sents there is no number
+        System.out.println("0" + '0');
+        System.err.println((1+2 == 3));
+        // I think this is becaus it thinks it is .4 because of a bit
+        System.err.println((.1+.2 == .3));
+        System.err.println(Byte.SIZE);
+        Byte x = Byte.MAX_VALUE;
+        System.err.println(x);
+        // This happens because of overloading
+        System.err.println(++x);
     }
 }
